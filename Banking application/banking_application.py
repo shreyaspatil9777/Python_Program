2ccb0ea9f08e08f8d3f177f16470dbd94438372d
shreyaspatil9777 <shreyasp67.sp@gmail.com>
2020-08-26 11:42:43 +0530

    Update banking_application.py

--- Banking application/banking_application.py
+++ Banking application/banking_application.py
@@ -10,7 +10,7 @@ class Account:
     type=""
 
     def createAccount(self):
-        self.accNo= int(input("Enter Account Number : "))
+        self.accountnumber= int(input("Enter Account Number : "))
         self.name= input("Enter Account Holder Name : ")
         self.type= input("Enter Account Type Current or Saving : ")
         self.deposite= int(input("Enter Amount : "))
@@ -18,7 +18,42 @@ class Account:
 
     def showAccount(self):
 
-        print("Account Number :",self.accNo)
+        print("Account Number :",self.accountnumber)
         print("Account Holder name :",self.name)
         print("Type of account :",self.type)
         print("Balance :",self.deposite)
+
+
+    def modifyAccount(self):
+        print("Account Number :",self.accountnumber)
+        self.name= input("Modify Account Holder Name : ")
+        self.type= input("Modify Account Type Current or Saving : ")
+        self.deposite= int(input("Modify Balance : "))
+
+    def depositeAccount(self,amount):
+        self.deposite +=amount
+
+    def withdrawAmount(self,amount):
+        self.deposite -=amount
+
+    def report(self):
+        print(self.accountnumber, "",self.name ,"",self.type,"",self.deposite)
+
+    def getAccount(self):
+        return self.accountnumber
+
+    def getAccountHolderName(self):
+        return self.name
+
+    def getDeposite(self):
+        return self.deposite
+
+def introduction():
+    print("\t\t#####################################")
+    print("\t\t\tBANK MANAGEMENT SYSTEM")
+    print("\t\t#####################################")
+    print("\t\t\t by Shreyas patil")
+    input()
+
+introduction()
+
